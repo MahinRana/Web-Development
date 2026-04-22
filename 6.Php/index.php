ba2385f91e8f2fc1ae573c08c6c8a8aa3324a121
MahinRana <mfaatahrana@gmail.com>
2026-04-23 00:14:24 +0500

    Add files via upload

---
+++ 6.Php/index.php
@@ -0,0 +1,13 @@
+<?php
+// This is a PHP comment
+
+// This line outputs text to the browser
+echo "Hello, World!";
+
+// Variable declaration and usage
+$name = "Alice";
+echo "Hello, " . $name;
+?>
+
+
+You can write HTML here as well... 
